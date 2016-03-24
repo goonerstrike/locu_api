3f56f9b275c84a8a9e9a4f2ccdb92cfb6fd0e024
Ryan Chiera <ryan.chiera@gmail.com>
2016-03-24 16:32:26 -0400

    blah

--- locu_api_example.py
+++ locu_api_example.py
@@ -2,21 +2,24 @@
 
 import urllib2
 import json
+import sys
 
 locu_api = '0db6690436828ac595096df808e7222e979259da'
 
 #url = 'https://api.locu.com/v1_0/venue/search/?radius=5000&locality=santa%20monica&category=restaurant&api_key=0db6690436828ac595096df808e7222e979259da'
+query = sys.argv[1]
 
 # Function to create URL to search from locu API
-def locu_search(query):
+def locu_search(city):
 	api_key = locu_api
 	# Attach api key to base URL
 	url = 'https://api.locu.com/v1_0/venue/search/?radius=5000'
 	# Replace the spaces of the query variable with %20 to insert into the final URL
-	locality = query.replace(' ', '%20')
+	locality = city.replace(' ', '%20')
 	final_url = url + "&locality=" + locality + "&category=restaurant" + "&api_key=" + api_key
 	json_obj = urllib2.urlopen(final_url)
 	data = json.load(json_obj)
 	
 	for business in data['objects']:
 		print business['name'], business['phone']
+locu_search(query)
